5253d04dfaebbc2c93d6ade365d52d9f2a4914f4
lai0xn <onigenko12@gmail.com>
2024-09-08 17:52:23 +0100

    Initial commit

---
+++ internal/server/routes.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", s.HelloWorldHandler)
+
+	return e
+}
+
+func (s *Server) HelloWorldHandler(c echo.Context) error {
+	resp := map[string]string{
+		"message": "Hello World",
+	}
+
+	return c.JSON(http.StatusOK, resp)
+}
---
+++ internal/server/routes_test.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	resp := httptest.NewRecorder()
+	c := e.NewContext(req, resp)
+	s := &Server{}
+	// Assertions
+	if err := s.HelloWorldHandler(c); err != nil {
+		t.Errorf("handler() error = %v", err)
+		return
+	}
+	if resp.Code != http.StatusOK {
+		t.Errorf("handler() wrong status code = %v", resp.Code)
+		return
+	}
+	expected := map[string]string{"message": "Hello World"}
+	var actual map[string]string
+	// Decode the response body into the actual map
+	if err := json.NewDecoder(resp.Body).Decode(&actual); err != nil {
+		t.Errorf("handler() error decoding response body: %v", err)
+		return
+	}
+	// Compare the decoded response with the expected value
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("handler() wrong response body. expected = %v, actual = %v", expected, actual)
+		return
+	}
+}

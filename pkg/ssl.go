8fc1417bc06ddd176dd7ad6001bba9a5bc969c2e
YacineMK <merroukiyacine06@example.com>
2024-09-20 02:02:41 +0200

    [feat]: add ssl util

--- pkg/ssl.go
+++ pkg/ssl.go
@@ -1,2 +1,33 @@
 package pkg
 
+import (
+	"log"
+	"net/http"
+	"time"
+	"crypto/tls"
+)
+
+func Checkssl(url string ) bool {
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.TLS != nil {
+		log.Println("SSL certificate exists!")
+		return true
+	} else {
+		log.Println("No SSL certificate found.")
+		return false
+	}
+}
